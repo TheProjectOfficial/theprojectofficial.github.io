71a411164fac7ea7c79bfb8505816824f6c3e5f4
Kira <v6267363@gmail.com>
2025-03-16 01:12:19 +0100

    Add files via upload

---
+++ res/js/actor.js
@@ -0,0 +1,189 @@
+const audioSixty = new Audio('https://theprojectofficial.github.io/res/audio/sfx/speech_number_060_en.ogg');
+audioSixty.loop = false;
+
+// AUDIO AFTER NUMBER
+var remaining = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_remaining_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+    onend: function() {
+        remaining.stop();  // This will stop the audio when it finishes playing
+    }
+});
+
+// AUDIO AFTER NUMBER
+var minutes = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_minutes_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+// AUDIO AFTER NUMBER
+var seconds = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_seconds_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+//BLANK
+var blank = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/blank.mp3'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+
+// NUMBERS
+var one = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_001_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var two = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_002_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var three = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_003_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var four = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_004_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var five = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_005_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var six = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_006_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var seven = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_007_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var eight = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_008_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var nine = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_009_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var ten = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_010_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var eleven = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_011_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var twelve = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_012_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var thirteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_013_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var fourteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_014_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var fifteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_015_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var sixteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_016_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var seventeen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_017_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var eighteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_018_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var nineteen = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_019_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var twenty = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_020_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+});
+
+var sixty = new Howl({
+    src: ['https://theprojectofficial.github.io/res/audio/sfx/speech_number_060_en.ogg'],
+    volume: 1,
+    loop: false,
+    autoplay: false,
+    onend: function() {
+        sixty.stop();  // This will stop the audio when it finishes playing
+    }
+});
